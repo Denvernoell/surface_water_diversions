1c3da6f75e215051e838a2fced7a9f3f980b9f0b
Denver Noell <dnoell@ppeng.com>
2023-01-13 15:41:48 -0800

    Added some requirements for diversion

--- app.py
+++ app.py
@@ -1,3 +1,28 @@
 import streamlit as st
 
 st.title("Surface Water Diversion Check")
+
+# def check_condition(condition, value):
+
+# 	if condition:
+# 		st.success(message)
+# 	else:
+# 		st.error
+"only divert when Delta in excess conditions (must check CVP SWP Ops)"
+
+"there must not be any curtailments of Post 1914 appropriators on the Chowchilla River"
+
+"d. Hourly and 24 hour rolling mean flow of Chowchilla Bypass Gaging Station (Gage CBP)"
+
+"e. Hourly and 24 hour rolling mean flow of Eastside Bypass at El Nido (Gage ELN)"
+
+"f. Daily record of spillway discharge and control regulating discharge from Friant (Gage MIL)"
+
+"g. daily 90th percentile flow values published by USGS at Newman Gage (Jan1 -Mar 31)"
+
+"a. delta outflow is above 44,500 cfs"
+
+"b. The water year type is 'below normal', 'normal', or Wet based on bulletin 120"
+
+"only divert if Friant Dam is spilling uncrotrolled excess flows, or water is being relased ffrom Firant for Flood Control Purposes  (will need to look for press release or that conservation pool is encroached) "
+
