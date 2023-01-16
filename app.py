b3373c1b082c8d9aabb3397a58da2b10872efd8f
Denver Noell <dnoell@ppeng.com>
2023-01-16 14:35:28 -0800

    Added tabs

--- app.py
+++ app.py
@@ -6,7 +6,16 @@ import json
 import numpy as np
 from bs4 import BeautifulSoup
 
-st.title("Surface Water Diversion Check")
+st.set_page_config(
+	page_title="Aliso Surface Water Diversion Check",
+	layout="wide",
+	page_icon="https://www.waterboards.ca.gov/images/banner/web_menu_graphic_sb.png"
+	)
+st.title("Aliso Surface Water Diversion Check")
+
+
+pd.options.display.float_format = '{:,.0f}'.format
+
 # date = st.date_input("Date")
 # start_date = arrow.get(date)
 start_date = arrow.now().shift(days=-2)
@@ -26,52 +35,41 @@ def show_condition(condition, passes):
 			st.error("Fail")
 
 class CDEC_flow:
-	def __init__(self,station,sensors,dur_code,start,end):
+	def __init__(self,station,sensors,dur_code,start_date=start_date,end_date=end_date):
+		start = start_date.format('YYYY-MM-DD')
+		end = end_date.format('YYYY-MM-DD')
+		self.station = station
+		self.sensors = sensors
 
 		url = f'https://cdec.water.ca.gov/dynamicapp/req/JSONDataServlet?Stations={station}&SensorNums={sensors}&dur_code={dur_code}&Start={start}&End={end}'
 		R = requests.get(url)
 		J = json.loads(R.text)
 
 		# This creates the dataframe and drops the last row
-		self.flow = pd.DataFrame(J)[:-1]
+		# df = pd.DataFrame(J)[:-1]
+		df = pd.DataFrame(J)
+
 		if dur_code == "H":
-			self.flow = self.clean_hourly()
+			df = pd.DataFrame(J)[:-1]
+			# ! excel export and json export are shifted by 1
+			# df = df.shift(1, freq='H')
 		elif dur_code == "D":
-			self.flow = self.clean_daily()
+			df = pd.DataFrame(J)[1:]
 
-
-	def clean_daily(self):
-		df = self.flow
 		df.index = pd.to_datetime(df['date'])
 		df['value'] = df['value'].where(df['value']>-9990,np.nan)
-		return df
+		self.flow = df
 
-	def clean_hourly(self):
+	def display(self):		
+		st.markdown(f"**{self.station}**")
+		# Date station id, flow, units
 		df = self.flow
-		df.index = pd.to_datetime(df['date'])
-		# ! excel export and json export are shifted by 1
-		df = df.shift(1, freq='H')
-		df['value'] = df['value'].where(df['value']>-9990,np.nan)
-		return df
+		# df['value'] = df['value'].map('${:,.0f}'.format)
+		# df['value'] = df['value'].astype(float).map('${:,.0f}'.format)
 
-
-
-def display_cdec(station,sensors,dur_code):
-	st.markdown(f"**{station}**")
-	flow = CDEC_flow(
-		station,
-		sensors,
-		dur_code,
-		start_date.format('YYYY-MM-DD'),
-		end_date.format('YYYY-MM-DD')).flow
-
-	st.dataframe(flow)
-	if sensors == "20":
-		st.markdown(f"**Rolling Average = {flow['value'].mean():.2f}**")
-	return flow
-
-
-
+		st.dataframe(df[['value']].style.format("{:,.0f}"))
+		if self.sensors == "20":
+			st.markdown(f"**Rolling Average = {self.flow['value'].mean():,.0f}**")
 
 
 def get_USGS_flow(site,start_date, end_date):
@@ -117,7 +115,7 @@ def get_water_year_classification():
 	df.columns = df.iloc[0]
 	df = df.iloc[[2,3]]
 
-	WY_index = df.loc[df['Forecast Date'] == 'Jan 1, 2023']['  90%'].iloc[0]
+	WY_index = df.loc[df['Forecast Date'] == 'Jan 1, 2023']['  75%'].iloc[0]
 	if float(WY_index) > 2.5:
 		return True
 	else:
@@ -138,106 +136,131 @@ def get_curtailment_status():
 	return manual_c == automatic_c == ['Not Curtailed']
 
 
+Checks, Operations, Conditions = st.tabs(["Checks", "Operations", "Conditions"])
 
-show_condition(
-	"period of 1/1/ 2023 to 6/14/2023",
-	arrow.get('2023-01-01','YYYY-MM-DD') < end_date < arrow.get("2023-06-14","YYYY-MM-DD"),
+with Checks:
+	newman_percent_flow = get_90th_percentile_flow(
+		start_date="1943-10-01",
+		end_date="2022-06-05",
+		percentile=90
+		)
+	newman_90 = newman_percent_flow.pipe(lambda df:df.loc[
+		(df['month_nu'] == end_date.format('M'))
+		& (df['day_nu'] == end_date.format('D'))
+		])['p90_va'].astype(float).iloc[0]
+	# Get newman and remove last row
+	newman = get_USGS_flow(
+		"11274000",
+		start_date,
+		end_date,
+	)[:-1]
+
+
+	newman['15012_00060'] = newman['15012_00060'].astype(float)
+	newman.index = newman['datetime']
+	newman_average = newman['15012_00060'].mean()
+		
+
+	DTO = CDEC_flow("DTO","23","D")
+	ELN = CDEC_flow("ELN","20","H")
+
+	CBP = CDEC_flow("CBP","20","H")
+
+	GRF = CDEC_flow("GRF","20","H")
+	SJB = CDEC_flow("SJB","20","H")
+
+	CBP_flow = GRF.flow['value'] - SJB.flow['value']
+	MIL_spill = CDEC_flow("MIL","71","D")
+	MIL_regulated = CDEC_flow("MIL","85","D")
+
+
+
+	show_condition(
+		"period of 1/1/ 2023 to 6/14/2023",
+		arrow.get('2023-01-01','YYYY-MM-DD') < end_date < arrow.get("2023-06-14","YYYY-MM-DD"),
+		)
+	# Uncomment this until April 1 comes out
+	# show_condition(
+	# 	"b. The water year type (April 1) is 'below normal', 'normal', or Wet based on bulletin 120",
+	# 	get_water_year_classification()
+	# )
+
+	show_condition(
+		"There must not be any curtailments of Post 1914 appropriators on the Chowchilla River",
+		get_curtailment_status()
 	)
 
-show_condition(
-	"b. The water year type is 'below normal', 'normal', or Wet based on bulletin 120",
-	get_water_year_classification()
-)
 
-show_condition(
-	"There must not be any curtailments of Post 1914 appropriators on the Chowchilla River",
-	get_curtailment_status()
-)
+	show_condition(
+		"c. Newman Gage (11274000) instantaneous of mean flow for previous 24 hour period greater than the published daily 90th percentile for 1/1 through 3/31",
+		newman_average > newman_90,	
+		)
 
+	show_condition("a. delta outflow is above 44,500 cfs",DTO.flow['value'].iloc[0] > 44500)
 
-newman_percent_flow = get_90th_percentile_flow(
-	start_date="1912-05-01",
-	end_date="2022-06-05",
-	percentile=90
-	)
+	show_condition(
+		"b. ELN instantaneous of mean flow for pervious 24 hour period greater than or equal 1297 CFS.",
+		ELN.flow['value'].mean() >= 1297
+		)
 
-
-newman_90 = newman_percent_flow.pipe(lambda df:df.loc[
-	(df['month_nu'] == end_date.format('M'))
-	& (df['day_nu'] == end_date.format('D'))
-	])['p90_va'].astype(float).iloc[0]
-
-with st.expander("g. daily 90th percentile flow values published by USGS at Newman Gage (Jan1 -Mar 31)"):
-	st.markdown(newman_90)
-
-newman = get_USGS_flow(
-	"11274000",
-	start_date,
-	end_date,
-)
-newman_average = newman['15012_00060'].astype(float).mean()
-with st.expander("c. Hourly and 24 hour rolling mean flow of SJR at Newman (Gage 11274000)"):
-	st.dataframe(newman)
-	st.markdown(f"**Rolling Average = {newman['15012_00060'].mean():.2f}**")
+	show_condition(
+		"a. CBP instantaneous of mean flow for previous 24 hour period greater than or equal 1938 CFS.",
+		# CBP.flow['value'].mean() >= 1938
+		CBP_flow.mean() >= 1938
+		)
 
 
-show_condition(
-	"c. Newman Gage (11274000) instantaneous of mean flow for previous 24 hour period greater than the published daily 90th percentile for 1/1 through 3/31",
-	newman_average > newman_90,	
-	)
+with Operations:
 
+	st.markdown("## Diversion Conditions")
+	"No more than 10,000 AF in season"
 
-# st.markdown("## Delta Outflow (DTO)")
-flow = display_cdec("DTO","23","D")
-show_condition("a. delta outflow is above 44,500 cfs",flow['value'].iloc[0] > 44500)
+	# "l. 20% calculation of daily flow at CBP gage for Jan 1 to March 31"
+	# "a. Diversion does not exceed 20% of daily flow at CBP" 
+	"Max flow rate is no more than the lower of 100 CFS or 20% of CBP flow"
+	CBP_20 = CBP_flow.mean() * .20
+	st.markdown(f"20% of CBP flow = {CBP_20:,.0f} CFS")
 
-
-with st.expander("e. Hourly and 24 hour rolling mean flow of Eastside Bypass at El Nido (Gage ELN)"):
-	ELN = display_cdec("ELN","20","H")
-
-show_condition(
-	"b. ELN instantaneous of mean flow for pervious 24 hour period greater than or equal 1297 CFS.",
-	ELN['value'].mean() >= 1297
-	)
-
-"only divert when Delta in excess conditions (must check CVP SWP Ops)"
+	st.metric("Max flow rate", f"{min(100,CBP_20):,.0f} CFS")
 
 
 
+	# CBP_20 = CDEC_flow(
+	# 		"CBP",
+	# 		"20",
+	# 		"H",
+	# 		arrow.get('Jan 1,2023',"MMM D,YYYY").format('YYYY-MM-DD'),
+	# 		arrow.get('Mar 31, 2023',"MMM D, YYYY").format('YYYY-MM-DD')).flow
+	# # st.dataframe(flow)
 
-with st.expander("d. Hourly and 24 hour rolling mean flow of Chowchilla Bypass Gaging Station (Gage CBP)"):
-	CBP = display_cdec("CBP","20","H")
+	# st.markdown(flow['value'].mean() * 0.2)
 
-show_condition(
-	"a. CBP instantaneous of mean flow for previous 24 hour period greater than or equal 1938 CFS.",
-	CBP['value'].mean() >= 1938
-	)
+	# st.markdown(CBP_flow.columns)
 
-with st.expander("f. Daily record of spillway discharge and control regulating discharge from Friant (Gage MIL)"):
-	display_cdec("MIL","71","D")
-	display_cdec("MIL","85","D")
+with Conditions:
+	with st.expander("Delta Outflow"):
+		DTO.display()
+	with st.expander("e. Hourly and 24 hour rolling mean flow of Eastside Bypass at El Nido (Gage ELN)"):
+		ELN.display()
 
-st.markdown("## Diversion Conditions")
-
-"no more than 100 CFS"
-"no more than 10,000 AF"
+	with st.expander("g. daily 90th percentile flow values published by USGS at Newman Gage (Jan1 -Mar 31)"):
+		st.markdown(f"{newman_90:,.0f} CFS")
 
 
-"l. 20% calculation of daily flow at CBP gage for Jan 1 to March 31"
-flow = CDEC_flow(
-		"CBP",
-		"20",
-		"H",
-		arrow.get('Jan 1,2023',"MMM D,YYYY").format('YYYY-MM-DD'),
-		arrow.get('Mar 31, 2023',"MMM D, YYYY").format('YYYY-MM-DD')).flow
-# st.dataframe(flow)
-st.markdown(flow['value'].mean() * 0.2)
-
-"a. Diversion does not exceed 20% of daily flow at CBP" 
-st.markdown(f"Allowed diversion = {CBP['value'].mean() * .20}")
+	with st.expander("c. Hourly and 24 hour rolling mean flow of SJR at Newman (Gage 11274000)"):
+		st.dataframe(newman[["15012_00060"]].style.format("{:,.0f}"))
+		st.markdown(f"Rolling Average = {newman_average:,.0f} CFS")
 
 
 
-"only divert if Friant Dam is spilling uncrotrolled excess flows, or water is being relased ffrom Firant for Flood Control Purposes  (will need to look for press release or that conservation pool is encroached) "
+	with st.expander("d. Hourly and 24 hour rolling mean flow of Chowchilla Bypass Gaging Station (Gage CBP)"):
+		CBP.display()
 
-"a. delta outflow is above 44,500 cfs"
+	with st.expander("CBP is down. Use GRF - SJR"):
+		GRF.display()
+		SJB.display()
+
+
+	with st.expander("f. Daily record of spillway discharge and control regulating discharge from Friant (Gage MIL)"):
+		MIL_spill.display()
+		MIL_regulated.display()
